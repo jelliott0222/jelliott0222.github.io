afc44b17f3e8dba033fd88d60e74802117028247
Jon Elliott <jelliott0222@gmail.com>
2018-05-06 23:16:27 -0400

    Complete project.

--- SecretAgentSupply/resources/js/mobilemenu.js
+++ SecretAgentSupply/resources/js/mobilemenu.js
@@ -1,6 +1,20 @@
 $(function() {
   $(".toggle-menu").click(function() {
-    $(this).parent().children('ul').toggleClass('active');
+    $(this).parent().find('.nav-menu').toggleClass('active');
+    return false;
+  });
+});
+
+$(function() {
+  $('.view-more-details').click(function() {
+    $(this).parent().find('.class-more-details').toggleClass('active');
+    return false;
+  });
+});
+
+$(function() {
+  $('.supplies-close-button').click(function() {
+    $(this).parent().toggleClass('active');
     return false;
   });
 });
