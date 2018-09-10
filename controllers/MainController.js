b3a2f8ec55e68ac8b6f0ab97dd1cca2631f32ae6
Jon Elliott <jelliott0222@gmail.com>
2018-09-10 00:45:32 -0400

    Content publish. Non-responsive layout prepared for first UAT.

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -6,7 +6,11 @@
 
     $http.get('resources/js/projects.json').then(function(response) {
       $scope.projects = response.data;
-    })
+    });
+
+    $http.get('resources/js/resume.json').then(function(response) {
+      $scope.resume = response.data;
+    });
 
   };
 
