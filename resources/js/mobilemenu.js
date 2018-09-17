90e5d68b2011939f5c0eab8a54d50b667ccaad46
Jon Elliott <jelliott0222@gmail.com>
2018-09-16 23:32:04 -0400

    Complete final rough draft pre-UAT.

--- resources/js/mobilemenu.js
+++ resources/js/mobilemenu.js
@@ -1,6 +1,12 @@
 $(function() {
   $(".toggle-menu").click(function() {
-    $(this).parent().children('ul').toggleClass('active');
+    $(this).parent().children('nav').toggleClass('active');
+    $(this).toggleClass('active');
     return false;
+  });
+
+  $("nav a").click(function() {
+    $(this).parent().toggleClass('active');
+    $(".toggle-menu").toggleClass('active');
   });
 });
