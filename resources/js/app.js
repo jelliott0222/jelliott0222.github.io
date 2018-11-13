ebfe4c283301b2104854b77c1b46b8942d5868a2
Jon Elliott <jelliott0222@gmail.com>
2018-11-12 22:17:25 -0500

    Update background and remove hashtags from URL.

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,13 +2,9 @@
 
   var app = angular.module('portfolio', ["ngRoute"]);
 
-  app.config(function($routeProvider) {
-    $routeProvider
-      .when('/home', {
-        templateUrl: 'resources/partials/home.html',
-        controller: 'MainController'
-      })
-      .otherwise({redirectTo: '/home'});
-  });
+  app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+
+
+  }]);
 
 }());
