76d7b700b4b8bcc9f93bb7c280d4b28c19f6b09a
Jon Elliott <jelliott0222@gmail.com>
2019-02-02 16:47:46 -0500

    Move locations jQuery into home.html partial.

--- projects/TeaCozy/app/app.js
+++ projects/TeaCozy/app/app.js
@@ -138,15 +138,6 @@
     };
   });
 
-  $('.location-tabs li a').click(function() {
-    $('.location-tabs li a').removeClass('active');
-    $(this).addClass('active');
-    var str = $(this).text();
-    str = str.replace(/\s/g,'');
-    var activeTab = '#' + str;
-    $('.location-container').removeClass('active');
-    $(activeTab).addClass('active');
-    return false;
-  });
+
 
 })();
