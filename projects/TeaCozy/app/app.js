50d4980d4ca6bf1b7420ead00a1016b4fa2a39ce
Jon Elliott <jelliott0222@gmail.com>
2019-02-02 16:38:53 -0500

    Remove onload from Locations jquery function.

--- projects/TeaCozy/app/app.js
+++ projects/TeaCozy/app/app.js
@@ -138,20 +138,15 @@
     };
   });
 
-  $(function() {
-    console.log($('.location-tabs'));
-    console.log($('.location-tabs li'));
-    console.log($('.location-tabs li a'));
-    $('.location-tabs li a').click(function() {
-      $('.location-tabs li a').removeClass('active');
-      $(this).addClass('active');
-      var str = $(this).text();
-      str = str.replace(/\s/g,'');
-      var activeTab = '#' + str;
-      $('.location-container').removeClass('active');
-      $(activeTab).addClass('active');
-      return false;
-    });
+  $('.location-tabs li a').click(function() {
+    $('.location-tabs li a').removeClass('active');
+    $(this).addClass('active');
+    var str = $(this).text();
+    str = str.replace(/\s/g,'');
+    var activeTab = '#' + str;
+    $('.location-container').removeClass('active');
+    $(activeTab).addClass('active');
+    return false;
   });
 
 })();
