0acc13dd95c90a4916e3346a0c938366c0dfd614
Jon Elliott <jelliott0222@gmail.com>
2019-02-02 16:35:32 -0500

    Move location tabs function to app.js and add watches.

--- projects/TeaCozy/app/app.js
+++ projects/TeaCozy/app/app.js
@@ -138,4 +138,20 @@
     };
   });
 
+  $(function() {
+    console.log($('.location-tabs'));
+    console.log($('.location-tabs li'));
+    console.log($('.location-tabs li a'));
+    $('.location-tabs li a').click(function() {
+      $('.location-tabs li a').removeClass('active');
+      $(this).addClass('active');
+      var str = $(this).text();
+      str = str.replace(/\s/g,'');
+      var activeTab = '#' + str;
+      $('.location-container').removeClass('active');
+      $(activeTab).addClass('active');
+      return false;
+    });
+  });
+
 })();
