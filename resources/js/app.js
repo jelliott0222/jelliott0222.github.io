0778931f74a1037fc8c9e2850c6dd1f400e205e9
Jon Elliott <jelliott0222@gmail.com>
2019-06-01 22:32:26 -0400

    Change age and add hashtag URL handling.

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,8 @@
   var app = angular.module('portfolio', ["ngRoute"]);
 
   app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-
+    // use the HTML5 History API
+    $locationProvider.html5Mode(true);
 
   }]);
 
